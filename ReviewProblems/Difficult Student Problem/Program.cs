f79632f306bf9cdcd89e8ce404129bccdfe4f504
shayo <shayo@DESKTOP-OPA1O5H>
2020-09-03 18:09:08 -0500

    difficult student problem

---
+++ ReviewProblems/Difficult Student Problem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Difficult_Student_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student(5, "Shayoni", "Banerjee", 40000);
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            myStudent.MakePayment(500);
+            Console.WriteLine(myStudent);
+
+            try
+            {
+                myStudent.MakePayment(-500);
+                Console.WriteLine(myStudent);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+        }
+    }
+}
---
+++ ReviewProblems/Difficult Student Problem/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Difficult_Student_Problem
+{
+    class Student
+    {
+        public int SoonerID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsOnProbation { get; set; }
+
+        public double GPA { get; set; }
+
+        private double BursarBalance;
+
+
+        /// <summary>
+        /// type ctor *tab tab* and it will autofill
+        /// </summary>
+        public Student()
+        {
+            SoonerID = -1;
+            FirstName = string.Empty;
+            LastName = "";
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 10000;
+
+        }
+
+        public Student(int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+
+        }
+        public void MakePayment(double amount)
+        {
+            if(amount > 0)
+            {
+                BursarBalance = BursarBalance - amount;
+            }
+            else
+            {
+                throw new Exception("INVALID PAYMENT AMOUNT");
+            }
+
+        }
+
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{FirstName} {LastName} ({SoonerID}) has a {GPA.ToString("N2")} GPA and owes {BursarBalance.ToString("C")}";
+           
+            if(IsOnProbation == true)
+            {
+                result += "\nIs on Probation";
+            }
+            else
+            {
+                result += "\nis not on Probation";
+            }
+                return result;
+        }
+    }
+}
